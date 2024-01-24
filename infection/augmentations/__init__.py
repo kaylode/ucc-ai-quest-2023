d541d3bd6260e8aab9cde1232449b362cb1a6d1e
kaylode <kayp.kieran@gmail.com>
2024-01-24 04:20:24 +0000

    :sparkles: Add different mean std stats

--- infection/augmentations/__init__.py
+++ infection/augmentations/__init__.py
@@ -2,9 +2,23 @@ from typing import *
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 from .mosaic import Mosaic
+import numpy as np
 
-def get_augmentations(split:str, image_size:int=512):
+ADE_MEAN = np.array([123.675, 116.280, 103.530]) / 255
+ADE_STD = np.array([58.395, 57.120, 57.375]) / 255
 
+IMAGENET_MEAN = np.array([0.485, 0.456, 0.406])
+IMAGENET_STD = np.array([0.229, 0.224, 0.225])
+
+def get_stats(stats:str='imagenet'):
+    if stats == 'imagenet':
+        return IMAGENET_MEAN, IMAGENET_STD
+    elif stats == 'ade':
+        return ADE_MEAN, ADE_STD
+    raise ValueError(f"Invalid stats name: {stats}")
+
+def get_augmentations(split:str, image_size:int=512, stats:str='imagenet'):
+    mean, std = get_stats(stats)
     train_augments = [
         A.Resize(int(image_size*1.25), int(image_size*1.25)),
         A.RandomCrop(image_size, image_size),
@@ -21,8 +35,8 @@ def get_augmentations(split:str, image_size:int=512):
             contrast_limit=0.3
         ),
         A.Normalize(
-            mean=[0.485, 0.456, 0.406],
-            std=[0.229, 0.224, 0.225]
+            mean=mean,
+            std=std
         ),
         ToTensorV2(),
     ]
@@ -30,16 +44,16 @@ def get_augmentations(split:str, image_size:int=512):
     val_augments = [
         A.Resize(image_size, image_size),
         A.Normalize(
-            mean=[0.485, 0.456, 0.406],
-            std=[0.229, 0.224, 0.225]
+            mean=mean,
+            std=std
         ),
         ToTensorV2(),
     ]
 
     test_augments = [
         A.Normalize(
-            mean=[0.485, 0.456, 0.406],
-            std=[0.229, 0.224, 0.225]
+            mean=mean,
+            std=std
         ),
         ToTensorV2(),
     ]
